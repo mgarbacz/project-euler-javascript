bb9a2fd4256fe0ce878457541b4a17daff604003
mgarbacz <mich.garbacz@gmail.com>
2013-01-31 20:54:08 -0600

    Solution beginning to form, but buggy. I will reason it out, and maybe switch to just reversing the product and comparing?

--- 004.js
+++ 004.js
@@ -8,9 +8,33 @@ function solution() {
   var num1 = 999;
   var num2 = 999;
 
+  // Well start checking palindromes on the product of num1 and num2
   var product = num1 * num2;
 
-  // Check if product is palindrome, if not decrement!
+  // This will be a math solution
+  var palindrome = false;
+  while (!palindrome) {
+    // Picking out the power of the first digit in the product
+    var powerStart
+      = Math.pow(10, Math.floor(Math.log(product + 1) / Math.LN10));
+    // Last digit is always 10
+    var powerEnd = 10;
+
+    // Assume we have a palindrome, change to false as soon as we see it is not
+    palindrome = true;
+    while (powerStart > powerEnd && palindrome) {
+      if (Math.floor(product / powerStart) != product % powerEnd)
+        palindrome = false;
+      // Move powers to check the next digits
+      powerStart /= 10;
+      powerEnd *= 10;
+    }
+    if (palindrome) console.log(product);
+    // The problems lies here I think!
+    num1 = num2;
+    num2--;
+    product = num1 * num2;
+  }
 
   return product;
 }
